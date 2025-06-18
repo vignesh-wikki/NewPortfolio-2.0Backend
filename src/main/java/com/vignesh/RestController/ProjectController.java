186a1309107be78e703ee22e54dfe91ca99cbc56
vignesh-wikki <vignesha445@gmail.com>
2025-06-18 13:22:20 +0530

    Added resume file for download

--- src/main/java/com/vignesh/RestController/ProjectController.java
+++ src/main/java/com/vignesh/RestController/ProjectController.java
@@ -14,7 +14,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 @Slf4j
-@CrossOrigin(origins = "https://wikki-portfolio.vercel.app/")
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/api")
 public class ProjectController {
